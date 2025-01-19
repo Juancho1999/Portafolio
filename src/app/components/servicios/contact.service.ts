04e8074edc97535228fa475799dc8c9ef574c022
Juancho <iralajua099@gmail.com>
2025-01-19 17:58:21 -0300

    Actualizar URL de la API en el servicio de contacto

--- src/app/components/servicios/contact.service.ts
+++ src/app/components/servicios/contact.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ContactService {
 
-  private apiUrl = 'https://portafolio-ten-vert-85.vercel.app/send-mail'; // Cambia esto a tu endpoint real
+  private apiUrl = 'https://portafolio-y8jgnqsv5-juans-projects-fd1bd95b.vercel.app/send-mail'; // Cambia esto a tu endpoint real
 
   constructor(private http: HttpClient) { }
 
