1459a1a652e7f400aa5d12d935ff30e822222a25
Juancho <iralajua099@gmail.com>
2025-02-03 01:37:38 -0300

    Actualizar URL de la API en el servicio de contacto a HTTPS

--- src/app/components/servicios/contact.service.ts
+++ src/app/components/servicios/contact.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ContactService {
 
-  private apiUrl = 'http://juanirala.vercel.app/sendMail.php'; // Asegúrate de cambiar la URL correcta // Cambia esto a tu endpoint real
+  private apiUrl = 'https://juanirala.vercel.app/sendMail.php'; // Asegúrate de cambiar la URL correcta // Cambia esto a tu endpoint real
 
   constructor(private http: HttpClient) { }
 
