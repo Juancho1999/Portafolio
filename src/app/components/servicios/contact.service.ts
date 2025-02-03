53889207af4fcf0a85fc9479d17b8694e64c4fea
Juancho <iralajua099@gmail.com>
2025-02-03 03:45:19 -0300

    Actualizar URL de la API en el servicio de contacto a HTTPS

--- src/app/components/servicios/contact.service.ts
+++ src/app/components/servicios/contact.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ContactService {
 
-  private apiUrl = 'http://back-juan.ct.ws/sendMail.php'; // Asegúrate de cambiar la URL correcta // Cambia esto a tu endpoint real
+  private apiUrl = 'https://back-juan.ct.ws/sendMail.php'; // Asegúrate de cambiar la URL correcta // Cambia esto a tu endpoint real
 
   constructor(private http: HttpClient) { }
 
