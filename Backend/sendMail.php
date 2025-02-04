1b53aef371a722aab519a5a29fab9a6f308a5de4
Juancho <iralajua099@gmail.com>
2025-02-04 11:50:35 -0300

    Actualizar métodos permitidos en el encabezado CORS en sendMail.php

--- Backend/sendMail.php
+++ Backend/sendMail.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
 header("Access-Control-Allow-Headers: Content-Type");
 
 use PHPMailer\PHPMailer\PHPMailer;
