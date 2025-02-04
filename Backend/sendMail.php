8a57fdadff33d82214c1d4047e947c14791e3960
Juancho <iralajua099@gmail.com>
2025-02-04 11:51:33 -0300

    Actualizar encabezado CORS en sendMail.php para permitir nuevos métodos y encabezados

--- Backend/sendMail.php
+++ Backend/sendMail.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
