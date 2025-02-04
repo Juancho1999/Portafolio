7f469b1ff608f65cb415daa5ae2852c51bace552
Juancho <iralajua099@gmail.com>
2025-02-04 12:04:15 -0300

    Agregar encabezado de tipo de contenido JSON y depuración en sendMail.php

--- Backend/sendMail.php
+++ Backend/sendMail.php
@@ -1,4 +1,6 @@
 <?php
+die(var_dump("Hola"));
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
